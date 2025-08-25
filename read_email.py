02e52923cc3a896b411929f3788f54d7b9e12c7b
DESKTOP-U9NI0SS\PC <adelin.mihaila@rinf.tech>
2025-08-25 19:12:12 +0300

    Updated messages load

--- read_email.py
+++ read_email.py
@@ -1,7 +1,7 @@
 from imap_tools import MailBox
 import yaml
 
-with open("D:/gmail_reader/creds.yml") as f:
+with open("D:/CODING ATTEMPTS/gmail_reader/creds.yml") as f:
     content = f.read()
 
 my_creds = yaml.load(content, Loader=yaml.FullLoader)
@@ -11,7 +11,7 @@ user, password = my_creds["user"], my_creds["password"]
 emails_deleted = 0
 uids_to_delete = []
 
-keywords = ['aliexpress', 'tech expo', "application was sent to", "application was viewed", "unfortunately", "decided not to move forward", "decided to move forward with other candidates", 'noreply', 'no-reply', 'jobs', 'reducere', 'promotie', 'discount', 'sephora', 'carzz', 'bilete', 'greenpeace']
+keywords = ['aliexpress', 'tech expo', "application was sent to", "application was viewed", "unfortunately", "decided not to move forward", "decided to move forward with other candidates", 'noreply', 'no-reply', 'jobs', 'reducere', 'promotie', 'discount', 'sephora', 'carzz', 'bilete', 'greenpeace', "thank you for applying"]
 senders = [
     'jobs-noreply@linkedin.com', 'updates-noreply@linkedin.com',
     'ae-report-info05.a4@deals.aliexpress.com', 'ae-newsletter05.a0@deals.aliexpress.com',
@@ -22,7 +22,7 @@ senders = [
 
 with MailBox("imap.gmail.com").login(user, password) as mailbox:
     mailbox.folder.set("INBOX")
-    for msg in mailbox.fetch(limit=3000):
+    for msg in mailbox.fetch(limit=500):
 
         if any(kw in msg.subject.lower() or kw in msg.from_.lower() or kw in msg.text.lower() for kw in keywords) or msg.from_.lower() in senders:
             print("Matched:", msg.uid, msg.from_)
